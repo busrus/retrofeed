3358333d78f455a3b79550f3725f0749efa4af02
Russell Henry <russell@russellhenry.net>
2025-02-01 19:38:55 -0500

    serve plain text to curl

--- src/main/java/com/retrofeed/servlet/FeedServlet.java
+++ src/main/java/com/retrofeed/servlet/FeedServlet.java
@@ -30,48 +30,57 @@ public class FeedServlet extends HttpServlet {
             config.getMastodonToken()
         );
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
             
+        String userAgent = request.getHeader("User-Agent");
+        boolean isCurl = userAgent != null && userAgent.toLowerCase().contains("curl");
+        
+        if (isCurl) {
+            serveTextContent(response);
+        } else {
+            serveHtmlContent(response);
+        }
+    }
+    
+    private void serveTextContent(HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain; charset=UTF-8");
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("=== RetroFeed ===");
+            
+            List<String> feed = mastodonService.getFeed();
+            for (String post : feed) {
+                out.println("--------------");
+                out.println(post);
+            }
+            
+            out.println("==============");
+        }
+    }
+    
+    private void serveHtmlContent(HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
         
         try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 2.0//EN\">");
             out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">");
             out.println("<html>");
-            out.println("<head>");
-            out.println("<title>RetroFeed</title>");
-            out.println("</head>");
+            out.println("<head><title>RetroFeed</title></head>");
             out.println("<body>");
             
             out.println("<p><b>=== RetroFeed ===</b></p>");
             
-            if (!mastodonService.isConfigured()) {
-                out.println("<p>Error: Service not configured<br>");
-                out.println("Status: ERROR</p>");
-                return;
-            }
-            
-            try {
-                List<String> feed = mastodonService.getFeed();
-                
-                for (String post : feed) {
-                    out.println("<p>--------------</p>");
-                    // Replace newlines with <br> tags
-                    String formattedPost = post.replace("\n", "<br>");
-                    out.println("<p>" + formattedPost + "</p>");
-                }
-                
-            } catch (IOException e) {
-                out.println("<p>Error: Unable to fetch feed<br>");
-                out.println("Status: ERROR</p>");
-                e.printStackTrace();
+            List<String> feed = mastodonService.getFeed();
+            for (String post : feed) {
+                out.println("<p>--------------</p>");
+                out.println("<p>" + post.replace("\n", "<br>") + "</p>");
             }
             
             out.println("<p>==============</p>");
-            out.println("</body>");
-            out.println("</html>");
+            out.println("</body></html>");
         }
     }
 }
